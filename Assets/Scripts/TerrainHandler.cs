e58df7514375ad3371d81aad569daa3647d073a8
Fernand <fernand.veyrier@epitech.eu>
2015-11-13 23:21:33 +0100

    rotation

---
+++ Assets/Scripts/TerrainHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainHandler : MonoBehaviour {
+	
+	[SerializeField]
+	private float rotationVel = 50f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update () 
+	{
+		this.transform.RotateAround(this.transform.position, new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0), rotationVel);
+	}
+}
