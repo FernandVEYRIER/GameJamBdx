1f4793bb0de5961c8d24fa9cde8367e23881dbd4
Fernand <fernand.veyrier@epitech.eu>
2015-11-14 09:16:25 +0100

    herbe

--- Assets/Scripts/TerrainHandler.cs
+++ Assets/Scripts/TerrainHandler.cs
@@ -6,6 +6,8 @@ public class TerrainHandler : MonoBehaviour {
 	[SerializeField]
 	private float rotationVel = 50f;
 
+	private Vector3 currentMousePosition;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,6 +15,9 @@ public class TerrainHandler : MonoBehaviour {
 	
 	void Update () 
 	{
-		this.transform.RotateAround(this.transform.position, new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0), rotationVel);
+		//this.transform.RotateAround(this.transform.position, new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0), rotationVel);
+		if (Input.GetMouseButton(0))
+			this.transform.RotateAround(this.transform.position, new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X") , 0), Vector3.Distance(Input.mousePosition, currentMousePosition));
+		currentMousePosition = Input.mousePosition;
 	}
 }
